36602d9981e02534ee70be114bd2d2dd5ff6ab3d
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2016-12-21 11:47:32 +0100

    Update Welcome Index View spec

--- spec/views/welcome/index.html.haml_spec.rb
+++ spec/views/welcome/index.html.haml_spec.rb
@@ -1,5 +1,9 @@
+require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe "welcome/index.html.haml", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "displays Welcome text" do
+    render
+    expect(rendered).to match "Welcome to the finance tracker app"
+  end
 end
