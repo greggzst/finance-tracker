cefa891b0c48eda512da557e9507855a2cf63015
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-01-05 17:23:13 +0100

    Add Stock model and its functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
+
   get 'users/my_portfolio'
 
   devise_for :users
@@ -6,5 +8,6 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
