904c4048b36f33f954b243502fd3d1332cc4bf8d
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-01-06 11:14:29 +0100

    Add friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   def full_name
-    "#{first_name} #{last_name}".strip if (first_name || last_name)
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"
   end
 
